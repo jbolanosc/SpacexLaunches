1609f09531854569f5fd7cc84ac5eb87209897b0
jbolanosc <josue.carit@outlook.com>
2019-05-21 00:09:13 -0600

    finished

---
+++ public/precache-manifest.477aa07d1a06855121d0c9e02f851c53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a144076f03edd3408692aa69a5dbc3a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f388b228492f16fb0557",
+    "url": "/static/css/main.2680dfa7.chunk.css"
+  },
+  {
+    "revision": "3b3b26607ebd884e6236",
+    "url": "/static/js/2.d24b106a.chunk.js"
+  },
+  {
+    "revision": "f388b228492f16fb0557",
+    "url": "/static/js/main.1db3db6a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
